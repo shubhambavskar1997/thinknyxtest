ce1a7e4271473f699594e2a988ffea97c0df0b8d
Shubham Bavaskar <bavaskarshubham885@gmail.com>
2021-04-23 10:06:45 +0000

    Updated after Yes command

--- webserver.sh
+++ webserver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-yum install httpd 
+yum install -y httpd 
 echo "<h1>This is my First Web Page by Jenkins</h1>" > /var/www/html/index.html
 systemctl enable httpd
 systemctl start httpd
